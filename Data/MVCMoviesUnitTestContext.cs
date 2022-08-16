5d88da3ae22d5b8d9018255f29b781012d30b948
Navdeep Kaur <200488890@student.georgianc.on.ca>
2022-08-16 19:52:50 +0100

    First Commit

---
+++ Data/MVCMoviesUnitTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMoviesUnitTest.Models;
+
+namespace MVCMoviesUnitTest.Data
+{
+    public class MVCMoviesUnitTestContext : DbContext
+    {
+        public MVCMoviesUnitTestContext (DbContextOptions<MVCMoviesUnitTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMoviesUnitTest.Models.Movie> Movie { get; set; } = default!;
+    }
+}
